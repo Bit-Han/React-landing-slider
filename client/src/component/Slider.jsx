0aece75c92b6462f0290a04659a089a7e3ebc982
HyperCode <72990129+Bit-Han@users.noreply.github.com>
2024-01-22 17:30:56 +0100

    adding chart ui

--- client/src/component/Slider.jsx
+++ client/src/component/Slider.jsx
@@ -3,30 +3,75 @@ import { Bar } from "react-chartjs-2";
 import { Chart, registerables } from "chart.js";
 
 
+
+
 const Slider = () => {
 	const [portfolio, setPortfolio] = useState(null);
 	const [whatever, setWhatEver] = useState({});
 	Chart.register(...registerables);
 
 	const data = {
-		labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'October'],
+		labels: [
+			"Nigeria stocks",
+			"Foreign stocks",
+			"Tech stocks",
+			"Emerging stocks",
+			"Nigerian bonds",
+			"Foreign Bonds",
+			"Commodities",
+			"Real Estate",
+			"T bills",
+			"Alternatives",
+		],
 		datasets: [
 			{
-				label: 'Monthly Sales',
-				backgroundColor: 'rgba(75,192,192,0.2)',
-				borderColor: 'rgba(75,192,192,1)',
-				borderWidth: 1,
-				hoverBackgroundColor: 'rgba(75,192,192,0.4)',
-				hoverBorderColor: 'rgba(75,192,192,1)',
-				data: [65, 59, 80, 81, 56, 55, 40],
+				label: "Stocks",
+				backgroundColor: [
+					"rgba(255, 99, 132, 0.2)",
+					"rgba(54, 162, 235, 0.2)",
+					"rgba(255, 206, 86, 0.2)",
+					"rgba(75, 192, 192, 0.2)",
+					"rgba(153, 102, 255, 0.2)",
+					"rgba(255, 159, 64, 0.2)",
+				],
+				borderColor: [
+					"rgba(255, 99, 132, 1)",
+					"rgba(54, 162, 235, 1)",
+					"rgba(255, 206, 86, 1)",
+					"rgba(75, 192, 192, 1)",
+					"rgba(153, 102, 255, 1)",
+					"rgba(255, 159, 64, 1)",
+				],
+				borderWidth: 2,
+				hoverBackgroundColor: "rgba(75,192,192,0.4)",
+				hoverBorderColor: "rgba(75,192,192,1)",
+				data: [10 , 30, 4 , 50 , 16, 16 , 65, 13 , 50 ],
 			},
 		],
 	};
 
+	const options = {
+    indexAxis: 'y',
+    // Elements options apply to all of the options unless overridden in a dataset
+    // In this case, we are setting the border of each horizontal bar to be 2px wide
+    elements: {
+      bar: {
+        borderWidth: 1,
+      }
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'right',
+      },
+	}
+};
+
+
 	useEffect(() => {
 		const fetchData = async () => {
 			// remember you changed the port here
-			const response = await fetch("http://localhost:5001/risk-data");
+			const response = await fetch("http://localhost:5000/risk-data");
 			const result = await response.json();
 			setPortfolio(result);
 		};
@@ -42,29 +87,30 @@ const Slider = () => {
 	};
 	return (
 		<>
-			{portfolio && <div  className="w-[50vw]">
-				<input
-					type="range"
-					name=""
-					id=""
-					defaultValue={0}
-					min={0}
-					max={10}
-					onChange={handleChange}
-				/>
-				<div>{whatever.nigerian_stocks}</div>
-				<Bar
-					data={data}
-					width={'50vw'}
-					options={{
-						scales: {
-							y: {
-								beginAtZero: true,
-							},
-						},
-					}}
-				/>
-			</div>}
+			{portfolio && (
+				<div className="w-[50vw] border rounded-md">
+					<div className="flex justify-between items-center">
+						<span>{whatever.risk_score}</span> <p>Example Portflio</p>
+					</div>
+
+					<input
+						type="range"
+						name=""
+						id=""
+						defaultValue={0}
+						min={0}
+						max={10}
+						onChange={handleChange}
+					/>
+
+					<Bar
+						className=""
+						data={data}
+						//width={"25vw"}
+						options={options}
+					/>
+				</div>
+			)}
 		</>
 	);
 }
--- client/src/layout/Chartjs.jsx
+++ client/src/layout/Chartjs.jsx
@@ -4,9 +4,8 @@ import Slider from "../component/Slider";
 
 const Chartjs = () => {
   return (
-		<section className="wow fadeInUp relative -mt-[110px]">
-			<div className="grid grid-cols-2 md:grid-cols-4 gap-4 bg-white border border-gray-200 border-opacity-50 shadow-2xl rounded-xl">
-                <h1>This is where the chart goes </h1>
+		<section className="wow fadeInUp relative -mt-[110px] ">
+			<div className="grid grid-cols-2 md:grid-cols-4 gap-4 border border-gray-200 border-opacity-50 shadow-2xl rounded-xl">
 				<Slider />
 				</div>
 			
