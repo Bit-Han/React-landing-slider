25412e22f36be412615205b97beec2025b35980d
HyperCode <72990129+Bit-Han@users.noreply.github.com>
2024-01-23 02:01:07 +0100

    finishing to the page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,16 @@ import Footer from "./layout/Footer";
 import Hero from "./layout/Hero";
 import CalltoAction from "./layout/CalltoAction";
 import TaxPage from "./layout/TaxPage";
-import Chartjs from "./layout/Chartjs";
-//import DisplayCharts from "./layout/DisplayCharts";
 
 function App() {
 	return (
 		<div>
 			<Navbar />
-			<Banner />
-			<Chartjs />
+			<Banner />	
 			<Hero />
 			<CalltoAction />
 			<TaxPage />
 			<Footer />  
-			{/* <DisplayCharts /> */}
 		</div>
 	);
 }
